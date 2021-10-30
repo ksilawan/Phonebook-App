34419d3cef6cc5ea5b1fb01483988f714b9c19d7
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-10-30 22:38:53 +0800

    Added clickable textview on act1.xml and intent to move on to the act2.xml page

--- app/src/main/java/com/demo/phonebookapp/MainActivity.java
+++ app/src/main/java/com/demo/phonebookapp/MainActivity.java
@@ -1,14 +1,34 @@
 package com.demo.phonebookapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView= (TextView)findViewById(R.id.contact1txttv);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+
+                Toast.makeText(MainActivity.this, "You clicked sir ice", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
-}
+
+    }
