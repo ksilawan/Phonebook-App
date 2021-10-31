1fd4ef4d3d0e65b478afc159c07c96577fedc1b5
ksilawan <74664668+ksilawan@users.noreply.github.com>
2021-10-31 23:33:50 +0800

    all names are now clickable and are able to move to and fro both activities

--- app/src/main/java/com/demo/phonebookapp/MainActivity.java
+++ app/src/main/java/com/demo/phonebookapp/MainActivity.java
@@ -4,31 +4,125 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
+    TextView eischied;
+    TextView gonzaga;
+    TextView arcenal;
+    TextView francis;
+    TextView ngannou;
+    TextView dana;
+    TextView white;
+    TextView daniel;
+    TextView cormier;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView= (TextView)findViewById(R.id.contact1txttv);
-        textView.setOnClickListener(new View.OnClickListener() {
+        eischied = (TextView)findViewById(R.id.eischied);
+        gonzaga = (TextView)findViewById(R.id.gonzaga);
+        arcenal = (TextView)findViewById(R.id.arcenal);
+        francis = (TextView)findViewById(R.id.francis);
+        ngannou = (TextView)findViewById(R.id.ngannou);
+        dana = (TextView)findViewById(R.id.dana);
+        white = (TextView)findViewById(R.id.white);
+        daniel = (TextView)findViewById(R.id.daniel);
+        cormier = (TextView)findViewById(R.id.cormier);
+
+
+        eischied.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
                 startActivity(intent);
-
-                Toast.makeText(MainActivity.this, "You clicked sir ice", Toast.LENGTH_SHORT).show();
-
             }
         });
-    }
+
+
+        gonzaga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+        arcenal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        francis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        ngannou.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        dana.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        white.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        daniel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        cormier.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+
+
+
 
     }
+
+        }
+
+
+
--- app/src/main/java/com/demo/phonebookapp/MainActivity2.java
+++ app/src/main/java/com/demo/phonebookapp/MainActivity2.java
@@ -1,14 +1,28 @@
 package com.demo.phonebookapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        back = (Button) findViewById(R.id.button);
+
+    }
+
+    public void movebacktoact1(View view){
+        Intent previouspage = new Intent(MainActivity2.this, Activity.class);
+        startActivity(previouspage);
     }
 }
